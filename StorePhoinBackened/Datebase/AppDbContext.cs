df62bea217f6dbbba8841e7e42025fb9d18c070f
Karrar_Mohammed <karrarm2000@gmail.com>
2024-10-29 16:49:21 +0300

    Add project files.

---
+++ StorePhoinBackened/Datebase/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using StorePhoinBackened.Models;
+using System.Data.Entity;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>()
+            .ToTable("Products")
+            .HasKey(p => p.Id);
+
+        modelBuilder.Entity<Product>()
+            .Property(p => p.Name)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        modelBuilder.Entity<Product>()
+            .Property(p => p.Description)
+            .HasMaxLength(500);
+
+        modelBuilder.Entity<Product>()
+            .Property(p => p.Price)
+            .HasColumnType("decimal(18,2)");
+
+        modelBuilder.Entity<Product>()
+            .Property(p => p.Stock)
+            .IsRequired();
+    }
+}
---
+++ StorePhoinBackened/Repository/Serves/ProductServes.cs
@@ -0,0 +1,46 @@
+﻿namespace StorePhoinBackened.Repository.Serves;
+
+using StorePhoinBackened.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+public class ProductRepository
+{
+    private readonly List<Product> _products = new List<Product>();
+
+    public IEnumerable<Product> GetAllProducts()
+    {
+        return _products;
+    }
+
+    public  Product GetProductById(int id)
+    {
+        return  _products.FirstOrDefault(p => p.Id == id) !;
+    }
+
+    public void AddProduct(Product product)
+    {
+        _products.Add(product);
+    }
+
+    public void UpdateProduct(Product product)
+    {
+        var existingProduct = GetProductById(product.Id);
+        if (existingProduct != null)
+        {
+            existingProduct.Name = product.Name;
+            existingProduct.Description = product.Description;
+            existingProduct.Price = product.Price;
+            existingProduct.QuantityInStock = product.QuantityInStock;
+        }
+    }
+
+    public void DeleteProduct(int id)
+    {
+        var product = GetProductById(id);
+        if (product != null)
+        {
+            _products.Remove(product);
+        }
+    }
+}
